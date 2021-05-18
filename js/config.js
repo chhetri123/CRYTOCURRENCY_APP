1baac41f71ab84ec6895834fb5b3ba5a7d08fd9d
Chhetri Rocks <chhetridon123@gmail.com>
2021-05-18 21:21:17 +0545

    initial commit

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
-export const LIMIT_API_DATA_CALL=10;
-export const TIMEOUT_ERROR=3
-export const CLIENT_ID='P3tfNcM9evWUnVvrKZQGqurhB6yFJMGTBM6lyC2JyKz77OZvS28U8roEzuzWDCpP'
+export const LIMIT_API_DATA_CALL = 10;
+export const TIMEOUT_ERROR = 3;
+export const CLIENT_ID =
+  'P3tfNcM9evWUnVvrKZQGqurhB6yFJMGTBM6lyC2JyKz77OZvS28U8roEzuzWDCpP';
