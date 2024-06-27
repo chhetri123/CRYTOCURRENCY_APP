0e5ed08406ea2b4d5d97954541937d8a601a3fe0
Manish Chhetri <chhetridon123@gmail.com>
2024-06-27 14:59:28 +0545

    config file

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const LIMIT_API_DATA_CALL = 10;
 export const TIMEOUT_ERROR = 3;
 export const API_KEY = "5fb7c0f2-2a21-4758-943c-dea07fbf47de";
-export const INITAL_LIMIT=10
+export const INITIAL_LIMIT = 10;
